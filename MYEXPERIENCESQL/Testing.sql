09cf367bcb0904a87550237fd5763bbac0fab976
papandayantrizthan811-spec <papandayantrizthan811@gmail.com>
2026-06-17 10:03:22 +0800

    Add files via upload

---
+++ MYEXPERIENCESQL/Testing.sql
@@ -0,0 +1,11 @@
+-- -- SELECT *
+-- -- FROM employees
+-- -- WHERE salary > 30000
+
+-- UPDATE employees
+-- SET salary = 30000
+-- WHERE employee_id = 2
+
+SELECT *
+FROM employees
+WHERE salary > 30000
